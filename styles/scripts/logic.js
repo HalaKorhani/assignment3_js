38445650fa36c0112569ee436bc6a3a1117a4954
HalaKorhani <halakorhani2005@icloud.com>
2025-04-16 20:23:24 +0300

    generate new list item element and assign task text content

--- styles/scripts/logic.js
+++ styles/scripts/logic.js
@@ -61,8 +61,10 @@ class Bankaccount{
     const input = document.getElementById("taskInput");
     const taskText = input.value.trim();
     if (taskText === "") return;
-  }
   
+  const li = document.createElement("li");
+      li.textContent = taskText;
+
 }
   
 
