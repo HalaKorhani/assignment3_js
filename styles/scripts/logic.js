d1c82b48cdbe5592217b6803d373a04b89cc51b8
HalaKorhani <halakorhani2005@icloud.com>
2025-04-16 16:20:24 +0300

    add a function to calculate the average of each student

--- styles/scripts/logic.js
+++ styles/scripts/logic.js
@@ -3,3 +3,9 @@ const students = [
   { name: "maria", scores: [70, 68, 72] },
   { name: "nabiha", scores: [100, 100, 100] }
 ];
+function generateReports(students) {
+  return students.map(student => {
+    const average = Math.round(
+      student.scores.reduce((sum, score) => sum + score, 0) / student.scores.length
+    )});
+  }
